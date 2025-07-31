79c74c1648a362f6a937190d9dfdb1af55734b81
Mads-Peter <machri@phys.au.dk>
2025-07-31 11:55:04 +0200

    Set python version to >=3.11.9

--- src/bayes_nanospace2025/__init__.py
+++ src/bayes_nanospace2025/__init__.py
@@ -1,3 +1,10 @@
 from bayes_nanospace2025.tutorial.kernels import RadialBasis, Noise, Constant
 from bayes_nanospace2025.tutorial.gp import GaussianProcess
 from bayes_nanospace2025.tutorial.plot_gp import plot_gp_prediction
+
+class YourCodeHereError(NotImplementedError):
+    def __init__(self, message="You need to implement this part of the code!"):
+        super().__init__(message)
+
+def YourCodeHere(message="You need to implement this part"):
+    raise YourCodeHereError(message)
